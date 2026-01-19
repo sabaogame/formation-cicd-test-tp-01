6f6dcde4c7353f14cc327e542cfb0ef86e2be4f3
sabaogame <71887317+sabaogame@users.noreply.github.com>
2026-01-19 16:11:30 +0100

    step C2-3 : implements logic

---
+++ src/main/java/com/devops/cicd/PasswordPolicy.java
@@ -0,0 +1,32 @@
+package com.devops.cicd;
+
+public class PasswordPolicy {
+
+    public static boolean isStrong(String password) {
+        if (password.length() < 8) {
+            return false;
+        }
+
+        String specialChars = "~`!@#$%^&*()-_=+\\|[{]};:'\",<.>/?";
+        char currentCharacter;
+        boolean numberPresent = false;
+        boolean upperCasePresent = false;
+        boolean lowerCasePresent = false;
+        boolean specialCharacterPresent = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            currentCharacter = password.charAt(i);
+            if (Character.isDigit(currentCharacter)) {
+                numberPresent = true;
+            } else if (Character.isUpperCase(currentCharacter)) {
+                upperCasePresent = true;
+            } else if (Character.isLowerCase(currentCharacter)) {
+                lowerCasePresent = true;
+            } else if (specialChars.contains(String.valueOf(currentCharacter))) {
+                specialCharacterPresent = true;
+            }
+        }
+
+        return numberPresent && upperCasePresent && lowerCasePresent && specialCharacterPresent;
+    }
+}
